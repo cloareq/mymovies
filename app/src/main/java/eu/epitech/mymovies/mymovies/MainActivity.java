66d688c95b2a1c385981ce0f2cd538b0be266af5
quentin <quentincperso@gmail.com>
2017-04-20 12:24:06 +0200

    getting user name and id

--- app/src/main/java/eu/epitech/mymovies/mymovies/MainActivity.java
+++ app/src/main/java/eu/epitech/mymovies/mymovies/MainActivity.java
@@ -4,17 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     private LoginButton loginButton;
     private CallbackManager callbackManager;
+    private AccessToken token;
+    private String UserName;
+    private int UserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +34,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 System.out.println("CONNECTED");
-                String UserId = loginResult.getAccessToken().getUserId();
-                String UserToken = loginResult.getAccessToken().getToken();
+                token = loginResult.getAccessToken();
+                String UserToken = loginResult.getAccessToken().getToken(); //JE PENSE PAS QU'ON VA AVOIR BESOIN DE CA MAIS JE LE LAISSE POUR L'INSTANT
+                GraphRequest request = GraphRequest.newMeRequest(
+                        token,
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                JSONObject Response = response.getJSONObject();
+                                try {
+                                    UserName = Response.getString("name");
+                                    UserId = Response.getInt("id");
+                                    System.out.println(UserName);
+                                    System.out.println(UserId);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name");
+                request.setParameters(parameters);
+                request.executeAsync();
             }
             @Override
             public void onCancel() {
