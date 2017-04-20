76ca27b0017ff6de240ad79ba08918919938a610
quentin <quentincperso@gmail.com>
2017-04-20 11:50:31 +0200

    facebook login working

--- app/src/main/java/eu/epitech/mymovies/mymovies/MainActivity.java
+++ app/src/main/java/eu/epitech/mymovies/mymovies/MainActivity.java
@@ -1,13 +1,50 @@
 package eu.epitech.mymovies.mymovies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_main);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                System.out.println("CONNECTED");
+                String UserId = loginResult.getAccessToken().getUserId();
+                String UserToken = loginResult.getAccessToken().getToken();
+            }
+            @Override
+            public void onCancel() {
+                System.out.println("CANCEL");
+            }
+            @Override
+            public void onError(FacebookException error) {
+                System.out.println("ERROR");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
+
+
