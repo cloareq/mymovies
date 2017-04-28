37b8ebe6e86728bdb2be013e41ee1b75b94e1737
quentin <quentincperso@gmail.com>
2017-04-28 16:56:24 +0200

    go on movieactivy on click on the movie image

--- app/src/main/java/eu/epitech/mymovies/mymovies/Models/RVAdapter.java
+++ app/src/main/java/eu/epitech/mymovies/mymovies/Models/RVAdapter.java
@@ -1,5 +1,7 @@
 package eu.epitech.mymovies.mymovies.Models;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,14 +12,19 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import eu.epitech.mymovies.mymovies.HomeActivity;
+import eu.epitech.mymovies.mymovies.MovieActivity;
 import eu.epitech.mymovies.mymovies.R;
 
 public class RVAdapter extends RecyclerView.Adapter<RVAdapter.MovieViewHolder>{
 
     List<Movies> movies;
 
-    public RVAdapter(List<Movies> movies){
-        this.movies = movies;
+    Context context;
+
+    public RVAdapter(List<Movies> movies, Context context){
+        this.movies=movies;
+        this.context=context;
     }
 
     @Override
@@ -33,10 +40,20 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.MovieViewHolder>{
     }
 
     @Override
-    public void onBindViewHolder(MovieViewHolder movieViewHolder, int i) {
+    public void onBindViewHolder(final MovieViewHolder movieViewHolder, final int i) {
         movieViewHolder.movieTitle.setText(movies.get(i).title);
         movieViewHolder.movieResume.setText(movies.get(i).overview);
-        movieViewHolder.moviePicture.setImageResource(movies.get(i).photoId);
+        movieViewHolder.moviePicture.setImageBitmap(movies.get(i).photoId);
+
+        movieViewHolder.moviePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println(movies.get(i).title);
+                Intent intent = new Intent(context, MovieActivity.class);
+                context.startActivity(intent);
+            }
+        });
+
     }
 
     @Override
---
+++ app/src/main/java/eu/epitech/mymovies/mymovies/MovieActivity.java
@@ -0,0 +1,34 @@
+package eu.epitech.mymovies.mymovies;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+}
